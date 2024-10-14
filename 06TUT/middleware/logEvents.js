a9f3ce25224780f290d88794303de297bfc6ee32
Asyraaf Razak <m.asyaaf.ra2ak@gmail.com>
2024-10-14 10:19:28 +0800

    Middleware

--- 06TUT/middleware/logEvents.js
+++ 06TUT/middleware/logEvents.js
@@ -8,12 +8,12 @@ const path = require('path');
 const logEvents = async (message, logName) => {
     const dateTime = `${format(new Date(), 'yyyyMMdd\tHH:mm:ss')}`;
     const logItem = `${dateTime}\t${uuid()}\t${message}\n`;
-    console.log(logItem);
+    //console.log(logItem);
     try {
-        if (!fs.existsSync(path.join(__dirname, 'logs'))) {
-            await fsPromises.mkdir(path.join(__dirname, 'logs'));
+        if (!fs.existsSync(path.join(__dirname, '..', 'logs'))) {
+            await fsPromises.mkdir(path.join(__dirname, '..', 'logs'));
         }
-        await fsPromises.appendFile(path.join(__dirname, 'logs', logName), logItem);
+        await fsPromises.appendFile(path.join(__dirname, '..', 'logs', logName), logItem);
     } catch (err) {
         console.log(err);
     }
@@ -25,4 +25,10 @@ console.log(format(new Date(), 'yyyyMMdd\tHH:mm:ss'))
 console.log(uuid())
 */
 
-module.exports = logEvents;
+const logger = (req, res, next) => {
+    logEvents(`${req.method}\t${req.headers.origin}\t${req.url}`, 'reqLog.txt');
+    console.log(`${req.method} ${req.path}`);
+    next();
+}
+
+module.exports = { logger, logEvents};
