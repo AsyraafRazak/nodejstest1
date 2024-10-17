0ec01b909bdf239b1ebeca3f022647affe972e8d
Asyraaf Razak <m.asyraaf.ra2ak@gmail.com>
2024-10-17 16:55:49 +0800

    User Roles Authorization

--- 06TUT/controllers/registerController.js
+++ 06TUT/controllers/registerController.js
@@ -15,7 +15,10 @@ const handleNewUser = async (req, res) => {
         //encrypt the password
         //const hashedPwd = await bcrypt.hash(pwd, 10);
         //store the new user
-        const newUser = { "username": user, "password": pwd };
+        const newUser = { 
+            "username": user, 
+            "roles": {"User": 2001},
+            "password": pwd };
         usersDB.setUsers([...usersDB.users, newUser]);
         await fsPromises.writeFile(
             path.join(__dirname, '..', 'model', 'users.json'),
