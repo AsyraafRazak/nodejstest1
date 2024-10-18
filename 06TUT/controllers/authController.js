bd1736690285934062020161a6a7813e14ae59e6
Asyraaf Razak <m.asyraaf.ra2ak@gmail.com>
2024-10-18 10:56:38 +0800

    Intro to mongodb and mongoose

--- 06TUT/controllers/authController.js
+++ 06TUT/controllers/authController.js
@@ -4,7 +4,7 @@ const usersDB = {
 }
 
 const jwt = require('jsonwebtoken');
-require('dotenv').config();
+//require('dotenv').config();
 const fsPromises = require('fs').promises;
 const path = require('path');
 
