06748c2da07b05808cd0c2b306b019085eb54492
Asyraaf Razak <m.asyraaf.ra2ak@gmail.com>
2024-10-21 10:26:35 +0800

    Async CRUD operation

--- 06TUT/controllers/authController.js
+++ 06TUT/controllers/authController.js
@@ -1,17 +1,21 @@
+const User = require('../model/User');
+/*
 const usersDB = {
     users: require('../model/users.json'),
     setUsers: function (data) { this.users = data }
 }
+    */
 
 const jwt = require('jsonwebtoken');
 //require('dotenv').config();
-const fsPromises = require('fs').promises;
-const path = require('path');
+//const fsPromises = require('fs').promises;
+//const path = require('path');
 
 const handleLogin = async (req, res) => {
     const { user, pwd } = req.body;
     if (!user || !pwd) return res.status(400).json({ 'message': 'Username and password are required.' });
-    const foundUser = usersDB.users.find(person => person.username === user);
+    
+    const foundUser = await User.findOne({ username: user}).exec();
     if (!foundUser) return res.sendStatus(401); //Unauthorized 
     // evaluate password 
     // const match = await bcrypt.compare(pwd, foundUser.password);
@@ -27,7 +31,7 @@ const handleLogin = async (req, res) => {
                 }
             },
             process.env.ACCESS_TOKEN_SECRET,
-            { expiresIn: '30s' }
+            { expiresIn: '180s' }
         );
         const refreshToken = jwt.sign(
             { "username": foundUser.username },
@@ -35,14 +39,11 @@ const handleLogin = async (req, res) => {
             { expiresIn: '1d' }
         );
         // Saving refreshToken with current user
-        const otherUsers = usersDB.users.filter(person => person.username !== foundUser.username);
-        const currentUser = { ...foundUser, refreshToken };
-        usersDB.setUsers([...otherUsers, currentUser]);
-        await fsPromises.writeFile(
-            path.join(__dirname, '..', 'model', 'users.json'),
-            JSON.stringify(usersDB.users)
-        );
-        res.cookie('jwt', refreshToken, { httpOnly: true, sameSite: 'None', secure: true, maxAge: 24 * 60 * 60 * 1000 });
+        foundUser.refreshToken = refreshToken;
+        const result = await foundUser.save();
+        console.log(result);
+
+        res.cookie('jwt', refreshToken, { httpOnly: true, sameSite: 'None', maxAge: 24 * 60 * 60 * 1000 }); //secure: true,
         res.json({ accessToken });
     } else {
         res.sendStatus(401);
